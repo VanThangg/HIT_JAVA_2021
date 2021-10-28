9f7732caa9aec500cb9f6a0881051035f8c5bd1c
VanThangg <thang.nv2882@gmail.com>
2021-10-28 19:14:27 +0700

    add btvn b3

--- BTVN_B3/Bai_1/RunMain.java
+++ BTVN_B3/Bai_1/RunMain.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Bai_1;
 
-/**
- *
- * @author Thang02
- */
 public class RunMain {
     public static void main(String[] args) {
         Person person = new Person("Nguyễn Văn Thắng", 19, "Name", "Code ^^");
